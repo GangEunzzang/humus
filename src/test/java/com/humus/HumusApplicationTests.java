a18d8a1b03254e23ca073951881534a8caee2235
이강은 <rkddms123456@gmail.com>
2025-07-16 17:56:16 +0900

    init commit

---
+++ src/test/java/com/humus/HumusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.humus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumusApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
